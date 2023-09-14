93cb298f632da9ead10d1804ab90acc8f80409f8
Kilian Lieret <kilian.lieret@posteo.de>
2023-09-14 14:08:09 +0200

    Scripts via entry_points.console_scripts
    Closes #22

--- src/jekyll_relative_url_check/cli.py
+++ src/jekyll_relative_url_check/cli.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from pathlib import Path
 import sys
 
@@ -7,13 +7,17 @@ from jekyll_relative_url_check.html import HTMLRelativeURLHook
 from jekyll_relative_url_check.markdown import MarkdownRelativeURLHook
 
 
-def html_pre_commit(files: List[str]):
+def html_pre_commit(files: Union[List[str], None] = None):
+    if files is None:
+        files = sys.argv[1:]
     ret = HTMLRelativeURLHook().check_files(map(Path, files))
     if not ret:
         sys.exit(111)
 
 
-def markdown_pre_commit(files: List[str]):
+def markdown_pre_commit(files: Union[List[str], None] = None):
+    if files is None:
+        files = sys.argv[1:]
     ret = MarkdownRelativeURLHook().check_files(map(Path, files))
     if not ret:
         sys.exit(111)
